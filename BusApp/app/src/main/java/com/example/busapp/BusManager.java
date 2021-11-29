74452790b5b96918fab33611e1fb72d858be2199
BEMELON <hgdkim2@naver.com>
2021-11-29 17:50:50 +0900

    가데이터 임시적으로 추가

--- BusApp/app/src/main/java/com/example/busapp/BusManager.java
+++ BusApp/app/src/main/java/com/example/busapp/BusManager.java
@@ -21,17 +21,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BusManager {
-    /**
-     * 시내버스(빨강버스) API 연동을 위한 변수입니다.
-     * 5003번   228000182(노선 ID) 98(정류장 순번)
-     * 5001-1번 228000177(노선 ID) 73(정류장 순번)
-     * 5000B번  228000174(노선 ID) 110(정류장 순번)
-     */
-    private static final String CityBus_endPoint = "http://apis.data.go.kr/6410000/busarrivalservice/getBusArrivalList";
-    private static final String CityBus_serviceKey = BuildConfig.DATAS_CLIENT_SECRET;
-    private static final String CityBus_stationID = "228002023";
-    private static final String[] routeIDs = new String[]{"228000182", "228000177", "228000174"};
-
     private XMLParser parser = null;
     private String[] MJUSTATION_STATIONS = null;
     private String[] MJUSTATION_TIMEREQUIRE = null;
@@ -50,28 +39,75 @@ public class BusManager {
     }
 
     /**
+     * 명지대역 셔틀버스
+     * * 실시간으로 값을 받아옵니다
+     *
+     * @return [x, x, x, x, x,] int[]
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static int[] getStationRouteInfo() {
+        return getRouteInfo("https://yax35ivans.apigw.ntruss.com/mba/v1/OjJo45tmXK/json");
+    }
+
+
+    /**
+     * 시내방향 셔틀버스
+     * * 실시간으로 값을 받아옵니다.
+     *
+     * @return [x, x, x, x, x,] int[]
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static int[] getCityRouteInfo() {
+        return getRouteInfo("https://yax35ivans.apigw.ntruss.com/mba/v1/9KC9LrEB87/json");
+    }
+
+    /**
+     * 명지대역 셔틀버스
+     * * 과거 데이터에 의존하여 값을 받습니다
+     *
+     * @param time
+     * @return
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static int[] predictShuttleTime(String time) {
+        int[] routeInfo = getPastRouteInfo("https://yax35ivans.apigw.ntruss.com/mba/v1/14gvgz0L2D/json", time);
+        if (routeInfo == null) return new int[]{3, 1, 3, 4, 1, 1};
+        else return routeInfo;
+    }
+
+    /**
+     * 시내 버스 (진입로)
+     * * 과거 데이터에 의존하여 값을 받습니다
+     *
+     * @param time
+     * @return
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static int[] predictBusTime(String time) {
+        int[] routeInfo = getPastRouteInfo("https://yax35ivans.apigw.ntruss.com/mba/v1/UKRrahCJdv/json", time);
+        return routeInfo;
+    }
+
+    /**
      * 진입로(명지대 방향)에 도착하는 시내 버스 정보를 API를 통하여 가져옵니다.
+     *
      * @return 가장 빨리 도착하는 버스(minute)
      */
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public static Integer getClosestCityBus() {
-        String[] result = null;
-
+    public static int getClosestCityBus() {
         try {
-            String apiUrl = CityBus_endPoint + "?serviceKey=" + CityBus_serviceKey + "&stationId=" + CityBus_stationID;
-
-            // Open the connection
-            URL url = new URL(apiUrl);
+            URL url = new URL("https://yax35ivans.apigw.ntruss.com/mba/v1/TLAVUb32yo/json");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Content-type", "application/xml");
-
             BufferedReader rd;
             if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                XMLParser parser = new XMLParser(conn.getInputStream());
-                result = parser.getElementByName("predictTime1", false);
-//                Log.d("DBG", Arrays.toString(result));
+                InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
+                Stream<String> streamOfString = new BufferedReader(inputStreamReader).lines();
+                String streamToString = streamOfString.collect(Collectors.joining());
+                if (streamOfString.equals("")) return 9999;
+                else return Integer.parseInt(streamToString);
             } else {
                 rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
                 Log.d("ERROR", rd.readLine());
@@ -80,88 +116,9 @@ public class BusManager {
             Log.e("REST_API", "GET method failed: " + e.getMessage());
             e.printStackTrace();
         }
-
-        int min = 9999;
-        for (String _time: result) {
-            int time = Integer.parseInt(_time);
-            if (time < min) min = time;
-        }
-
-        if (min == 9999) return null;
-        else return min;
+        return 9999;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private static int[] getRouteInfo(String endPointUrl) {
-        try {
-            URL url = new URL(endPointUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-type", "application/json");
-
-            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
-                Stream<String> streamOfString= new BufferedReader(inputStreamReader).lines();
-                String streamToString = streamOfString.collect(Collectors.joining());
-
-                return Arrays.stream(
-                             new JSONObject(streamToString).getString("body").split(",")
-                            ).mapToInt(Integer::parseInt)
-                             .toArray();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    /**
-     * MJU-SHUTTLE: to STATION
-     * LIVE DATA
-     * @return int "x,x,x,x,x,x"
-     */
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public static int[] getStationRouteInfo() {
-        return getRouteInfo("https://yax35ivans.apigw.ntruss.com/mba/v1/OjJo45tmXK/json");
-    }
-
-    /**
-     * MJU-SHUTTLE: to CITY
-     * LIVE DATA
-     * @return int "x,x,x,x,x,x"
-     */
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public static int[] getCityRouteInfo() {
-        return getRouteInfo("https://yax35ivans.apigw.ntruss.com/mba/v1/9KC9LrEB87/json");
-    }
-
-    private static String toDayString(int idx) {
-        return new String[]{"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"}[idx];
-    }
-
-
-    /**
-     * MJU-SHUTTLE
-     * PAST DATA
-     * @param time
-     * @return
-     */
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public static int[] predictShuttleTime(String time){
-       return getPastRouteInfo("https://yax35ivans.apigw.ntruss.com/mba/v1/14gvgz0L2D/json", time);
-    }
-
-    /**
-     * BUS
-     * @param time
-     * @return
-     */
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public static int[] predictBusTime(String time) {
-        return getPastRouteInfo("https://yax35ivans.apigw.ntruss.com/mba/v1/UKRrahCJdv/json", time);
-    }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     private static int[] getPastRouteInfo(String endpoint, String time) {
@@ -182,7 +139,7 @@ public class BusManager {
 
             if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
                 InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
-                Stream<String> streamOfString= new BufferedReader(inputStreamReader).lines();
+                Stream<String> streamOfString = new BufferedReader(inputStreamReader).lines();
                 String streamToString = streamOfString.collect(Collectors.joining());
 
                 return Arrays.stream(
@@ -201,43 +158,75 @@ public class BusManager {
         return null;
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private static int[] getRouteInfo(String endPointUrl) {
+        try {
+            URL url = new URL(endPointUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
+                Stream<String> streamOfString = new BufferedReader(inputStreamReader).lines();
+                String streamToString = streamOfString.collect(Collectors.joining());
+
+                return Arrays.stream(
+                        new JSONObject(streamToString).getString("body").split(",")
+                ).mapToInt(Integer::parseInt)
+                        .toArray();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static String toDayString(int idx) {
+        return new String[]{"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"}[idx];
+    }
+
+
     public String[] BUS_MJUSTATION_STATIONS() {
-        if(MJUSTATION_STATIONS == null) {
+        if (MJUSTATION_STATIONS == null) {
             MJUSTATION_STATIONS = parser.getElementByName("BUS_MJUSTATION_STATIONS", true);
         }
         return MJUSTATION_STATIONS;
     }
 
     public String[] BUS_MJUSTATION_TIMEREQUIRE() {
-        if(MJUSTATION_TIMEREQUIRE == null) {
+        if (MJUSTATION_TIMEREQUIRE == null) {
             MJUSTATION_TIMEREQUIRE = parser.getElementByName("BUS_MJUSTATION_TIMEREQUIRE", true);
         }
         return MJUSTATION_TIMEREQUIRE;
     }
 
     public String[] BUS_MJUSTATION_TIMETABLE() {
-        if(MJUSTATION_TIMETABLE == null) {
+        if (MJUSTATION_TIMETABLE == null) {
             MJUSTATION_TIMETABLE = parser.getElementByName("BUS_MJUSTATION_TIMETABLE", true);
         }
         return MJUSTATION_TIMETABLE;
     }
 
     public String[] BUS_CITY_STATIONS() {
-        if(CITY_STATIONS == null) {
+        if (CITY_STATIONS == null) {
             CITY_STATIONS = parser.getElementByName("BUS_CITY_STATIONS", true);
         }
         return CITY_STATIONS;
     }
 
     public String[] BUS_CITY_TIMEREQUIRE() {
-        if(CITY_TIMEREQUIRE == null) {
+        if (CITY_TIMEREQUIRE == null) {
             CITY_TIMEREQUIRE = parser.getElementByName("BUS_CITY_TIMEREQUIRE", true);
         }
         return CITY_TIMEREQUIRE;
     }
 
     public String[] BUS_CITY_TIMETABLE() {
-        if(CITY_TIMETABLE == null) {
+        if (CITY_TIMETABLE == null) {
             CITY_TIMETABLE = parser.getElementByName("BUS_CITY_TIMETABLE", true);
         }
         return CITY_TIMETABLE;
